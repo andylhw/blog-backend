03f53aaff72966037529ecb439e27a9e11a99373
andylhw <andylhw12@soongsil.ac.kr>
2021-03-25 15:01:20 +0900

    added board/get + post put/post/get

--- src/main/java/com/alwayslearn/blog/contorller/BoardController.java
+++ src/main/java/com/alwayslearn/blog/contorller/BoardController.java
@@ -1,9 +1,16 @@
 package com.alwayslearn.blog.contorller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
+
+    @GetMapping("/{boardId)")
+    @ResponseStatus(HttpStatus.OK)
+    public void getBoards(@RequestParam(required = false) int page){
+
+    }
+
 }
--- src/main/java/com/alwayslearn/blog/contorller/PostController.java
+++ src/main/java/com/alwayslearn/blog/contorller/PostController.java
@@ -1,9 +1,27 @@
 package com.alwayslearn.blog.contorller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boards/{boardsId}/posts")
 public class PostController {
+
+    @GetMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void getPost(@PathVariable String postId) throws ChangeSetPersister.NotFoundException {
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void writePost(@RequestBody WritePostRequest writePostRequest){
+
+    }
+    @PutMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePost(@PathVariable Long postId ) throws ChangeSetPersister.NotFoundException {
+
+    }
 }
