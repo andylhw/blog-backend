23486344a20feb8e30ff0f4ef516167af4b434eb
andylhw <andylhw12@soongsil.ac.kr>
2021-03-25 15:31:13 +0900

    원본훼손패치 2

--- src/main/java/com/alwayslearn/blog/contorller/BoardController.java
+++ src/main/java/com/alwayslearn/blog/contorller/BoardController.java
@@ -1,5 +1,7 @@
 package com.alwayslearn.blog.contorller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
