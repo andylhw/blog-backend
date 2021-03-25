46acafb4071782e43d3667ae62a290c4abffbea1
DESKTOP-K0AJ7QS\박지민 <pjws0304@naver.com>
2021-03-25 15:09:21 +0900

    posts의 PATCH, DELETE comments의 GET

--- src/main/java/com/alwayslearn/blog/contorller/CommentController.java
+++ src/main/java/com/alwayslearn/blog/contorller/CommentController.java
@@ -1,9 +1,17 @@
 package com.alwayslearn.blog.contorller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/boards/{boardsId}/posts/{postId}/comments")
 public class CommentController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void getComments(@RequestParam(required = false) long size, @PathVariable String boardsId, @PathVariable String postId){
+
+    }
 }
