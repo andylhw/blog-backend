347ace034c3df1a2e95ce6523b81eb4f03aff4ab
DESKTOP-K0AJ7QS\박지민 <pjws0304@naver.com>
2021-03-25 15:12:21 +0900

    BoardController 초기화 (직접 작성한 부분만)

--- src/main/java/com/alwayslearn/blog/contorller/BoardController.java
+++ src/main/java/com/alwayslearn/blog/contorller/BoardController.java
@@ -6,10 +6,5 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
-
-    @GetMapping("/{boardId}")
-    @ResponseStatus(HttpStatus.OK)
-    public void getBoards(@PathVariable long boardId){
-
-    }
+    
 }
