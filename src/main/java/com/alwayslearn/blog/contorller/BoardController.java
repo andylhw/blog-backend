f6f0c18bad0afd34aacc6b9d4d826a4793ea60d2
CodingNome <si8363@naver.com>
2021-03-25 14:51:15 +0900

    feat: boardController

--- src/main/java/com/alwayslearn/blog/contorller/BoardController.java
+++ src/main/java/com/alwayslearn/blog/contorller/BoardController.java
@@ -1,9 +1,15 @@
 package com.alwayslearn.blog.contorller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
+
+    @GetMapping("/{boardId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void get(@PathVariable long boardId){
+
+    }
 }
